1c5c0d682dd0cd03c44302db093ca6cb43587e47
ElliotMoreau <73651345+cempack@users.noreply.github.com>
2022-02-13 12:36:25 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 echo "Getting Orchis Theme"
-sudo wget https://github.com/vinceliuice/Orchis-theme/archive/refs/heads/master.zip
+git clone https://github.com/vinceliuice/Orchis-theme/archive/refs/heads/master.zip
 echo "Installing Orchis Theme"
