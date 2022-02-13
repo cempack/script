18680a9e410924fba642066c2bd54773219d7faa
ElliotMoreau <73651345+cempack@users.noreply.github.com>
2022-02-13 12:35:05 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
-echo "Installing Gnome Extensions ..."
-./extensions.sh --enable --file extensions.txt
 echo "Getting Orchis Theme"
-wget https://github.com/vinceliuice/Orchis-theme/archive/refs/heads/master.zip
+sudo wget https://github.com/vinceliuice/Orchis-theme/archive/refs/heads/master.zip
 echo "Installing Orchis Theme"
