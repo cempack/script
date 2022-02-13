f83ac2c97efeeb4c50d5bd15bfc514452507d654
ElliotMoreau <73651345+cempack@users.noreply.github.com>
2022-02-13 17:05:28 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 echo "$(tput setaf 2)###########Installing Dependencies###########$(tput setaf 7)"
-sudo apt install git apt-transport-https curl
+sudo apt install git curl
 echo "$(tput setaf 2)###########Installing Themes###########$(tput setaf 7)$(tput setaf 7)"
 echo "$(tput setaf 2)Getting Orchis Theme$(tput setaf 7)"
 sudo git https://github.com/vinceliuice/Orchis-theme
@@ -28,6 +28,7 @@ sudo apt install lutris
 "$(tput setaf 2)Installing Brave Browser$(tput setaf 7)"
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt install apt-transport-https curl
 sudo apt update
 sudo apt install brave-browser
 echo "$(tput setaf 1)###########You Finished The Installation Now You Need To Install The Extentions In $(tput setaf 6)Extentions.txt $(tput setaf 1)And To Download The Extention For Ulauncher Who Are In $(tput setaf 6)Ulauncher.txt$(tput setaf 1)###########$(tput setaf 7)"
